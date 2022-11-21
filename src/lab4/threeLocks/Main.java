35cf6346c8ced30f80f14b9dd2f32de05ac1a57d
igsit <sigors100@gmail.com>
2022-11-21 15:48:42 +0100

    P/C problem made on three locks

---
+++ src/lab4/threeLocks/Main.java
@@ -0,0 +1,31 @@
+package lab4.threeLocks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int amount = 20;
+        int maxBuffer = 10;
+        int fatChange = 4;
+        Monitor monitor = new Monitor(maxBuffer);
+
+        List<Producer> producers = new ArrayList<>();
+        List<Client> clients = new ArrayList<>();
+
+        for (int i = 0; i < amount; i++) {
+            producers.add(new Producer(monitor, 1));
+            clients.add(new Client(monitor, 1));
+        }
+
+        producers.get(0).setProduction(fatChange);
+        clients.get(0).setConsumption(fatChange);
+
+        for (int i = 0; i < amount; i++) {
+            producers.get(i).start();
+            clients.get(i).start();
+        }
+    }
+
+
+}
---
+++ src/lab4/threeLocks/Monitor.java
@@ -0,0 +1,60 @@
+package lab4.threeLocks;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Monitor {
+    private Lock consumerLock = new ReentrantLock();
+    private Lock producerLock = new ReentrantLock();
+    private Lock innerLock = new ReentrantLock();
+    private Condition condition = innerLock.newCondition();
+    private int bufferCount = 0;
+    private final int maxBuffer;
+
+    public void consume(int consumption) {
+        consumerLock.lock();
+        innerLock.lock();
+        try {
+            while (!canConsume(consumption)) {
+                condition.await();
+            }
+            bufferCount -= consumption;
+            condition.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            consumerLock.unlock();
+            innerLock.unlock();
+        }
+    }
+
+    public void produce(int consumption) {
+        producerLock.lock();
+        innerLock.lock();
+        try {
+            while (!canProduce(consumption)) {
+                condition.await();
+            }
+            bufferCount += consumption;
+            condition.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            producerLock.unlock();
+            innerLock.unlock();
+        }
+    }
+
+    public Monitor(int maxBuffer) {
+        this.maxBuffer = maxBuffer;
+    }
+
+    private boolean canProduce(int production) {
+        return bufferCount + production <= maxBuffer;
+    }
+
+    private boolean canConsume(int consumption) {
+        return bufferCount > consumption;
+    }
+}
